1de4bb667c6ecb262fbd78bd7b6a769111e8e8cd
Bagas Hananto <bagashp01@gmail.com>
2023-12-04 21:03:01 +0700

    {Merapihkan detail-detail}

--- src/pages/Barbers/Barbers.jsx
+++ src/pages/Barbers/Barbers.jsx
@@ -6,16 +6,6 @@ const Barbers = () => {
 			<section>
 				<div className="container text-center">
 					<h2 className="heading">Pilihlah Barber Sesukamu</h2>
-					<div className="max-w-[570px] mt-[22px] mx-auto bg-[#0066FF2c] rounded-md flex items-center justify-between">
-						<input
-							type="search"
-							className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
-							placeholder="Search Barber"
-						/>
-						<button className="btn mt-0 rounded-[0px] rounded-r-md">
-							Cari
-						</button>
-					</div>
 				</div>
 			</section>
 
