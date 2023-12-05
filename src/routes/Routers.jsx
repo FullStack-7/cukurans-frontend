2501df543e3f8c4e5fe58cc16117445d95cad4b8
Bagas Hananto <bagashp01@gmail.com>
2023-12-05 18:51:51 +0700

    {Fixing the router}

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -3,11 +3,10 @@ import Home from '../pages/Home';
 import Services from '../pages/Services';
 import Contact from '../pages/Contact';
 import Login from '../pages/Login';
-import BarberDashboard from '../dashboard/Barber/BarberDashboard';
+import BarberDashboard from '../dashboard/barber/BarberDashboard';
 import BarberDetails from '../pages/Barbers/BarberDetails';
 import SignUp from '../pages/SignUp';
 import Barbers from '../pages/Barbers/Barbers';
-import Payment from '../pages/Barbers/Payment.jsx';
 import UserDashboard from '../dashboard/user/UserDashboard';
 
 import { Routes, Route } from 'react-router-dom';
@@ -22,7 +21,6 @@ const Routers = () => {
 			<Route path="/contact" element={<Contact />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<SignUp />} />
-			<Route path="/payment" element={<Payment />} />
 			<Route path="/users/userprofile/:id" element={<UserDashboard />} />
 			<Route path="/dashboard-barber" element={<BarberDashboard />} />
 		</Routes>
